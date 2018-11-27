a607119e25922dadb25111a3e6dbd0a5c0fa1578
Nick Tang <nicktang@MacBook-Pro.local>
2018-11-27 18:39:04 -0500

    initial commit

---
+++ src/main/java/com/deidaraxc4/wouldyourather/WouldYouRatherApplication.java
@@ -0,0 +1,12 @@
+package com.deidaraxc4.wouldyourather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WouldYouRatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WouldYouRatherApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deidaraxc4/wouldyourather/WouldYouRatherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deidaraxc4.wouldyourather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WouldYouRatherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
