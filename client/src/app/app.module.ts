81712d82e981efea990cf5057cef9ea53d5009d1
Nick Tang <nick.tang@onedatascan.com>
2019-01-03 16:26:53 -0500

    created home component and added mat-toolbar in app-root

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PollComponent } from './poll/poll.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PollComponent
+    PollComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
