9b6dbf8d10cd943f9adb6d62c413d5770dcee19a
Nick Tang <nick.tang@onedatascan.com>
2019-01-04 15:47:30 -0500

    enabled cross origin requests so angular client can call api

--- src/main/java/com/deidaraxc4/wouldyourather/controller/WyrController.java
+++ src/main/java/com/deidaraxc4/wouldyourather/controller/WyrController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class WyrController {
@@ -17,6 +18,7 @@ public class WyrController {
     @Autowired
     WyrService wyrService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/questions", method = RequestMethod.GET)
     public List<WyrQuestion> findAllWyrQuestions() {
 	List<WyrQuestion> wyrQuestions = (List<WyrQuestion>) wyrService.findAll();
@@ -24,21 +26,25 @@ public class WyrController {
 	return wyrQuestions;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/question/{questionId}", method = RequestMethod.GET)
     public WyrQuestion findOneWyrQuestion(@PathVariable Long questionId) {
 	return wyrService.findById(questionId);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/questions", method = RequestMethod.POST)
     public WyrQuestion createWyrQuestion(@RequestBody WyrQuestion wyrQuestion) {
 	return wyrService.save(wyrQuestion);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "question/{questionId}", method = RequestMethod.DELETE)
     public void deleteWyrQuestionById(@PathVariable Long questionId) {
 	wyrService.deleteById(questionId);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "question/{questionId}", method = RequestMethod.PUT)
     public WyrQuestion voteWyrQuestion(@PathVariable Long questionId, @RequestParam(name="id",required=true) int id) {
 	return wyrService.vote(wyrService.findById(questionId),id);
