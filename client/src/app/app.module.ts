5093c4e92a47e33777c1866513c78bd2295cc8eb
Nick Tang <nick.tang@onedatascan.com>
2019-01-04 14:36:04 -0500

    initial create question form view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,23 +4,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { PollComponent } from './poll/poll.component';
 import { HomeComponent } from './home/home.component';
+import { CreateQuestionComponent } from './create-question/create-question.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PollComponent,
-    HomeComponent
+    HomeComponent,
+    CreateQuestionComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/create-question/create-question.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-question',
+  templateUrl: './create-question.component.html',
+  styleUrls: ['./create-question.component.css']
+})
+export class CreateQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submit(value: any) {
+    console.log(value);
+  }
+
+}
