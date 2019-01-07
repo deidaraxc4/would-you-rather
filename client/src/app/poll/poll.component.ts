bde4019cb31f40f87dbbd10fd76ff37966183cac
Nick Tang <nick.tang@onedatascan.com>
2019-01-07 16:19:21 -0500

    clean up debug code and comments

--- client/src/app/poll/poll.component.ts
+++ client/src/app/poll/poll.component.ts
@@ -9,18 +9,8 @@ import { Question } from '../vote-question/question.model';
 export class PollComponent implements OnInit {
   isVoted : boolean = false;
   @Input('question') question : Question;
-  // @Input('choice_a') choice_a_text : string = "test a";
-  // @Input('choice_b') choice_b_text: string = "test b";
-  // choice_a_votes: number = 1;
-  // choice_b_votes: number = 3;
 
   constructor() {
-    // delete this later you would get from vote question component
-    // this.question = new Question();
-    // this.question.choice_a_text = "pickles";
-    // this.question.choice_b_text = "lemons";
-    // this.question.choice_a_votes = 23;
-    // this.question.choice_b_votes = 12;
    }
 
   ngOnInit() {
@@ -29,6 +19,7 @@ export class PollComponent implements OnInit {
   toggleVoted() {
     this.isVoted = !this.isVoted;
     console.log("voted");
+    // call the PUT api here and update the question member variable
   }
 
 }
